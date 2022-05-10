a760d398334b0c96edd0ba8ec5841a83bef54e33
Matteo Bianchi <m.bianchi@opengate.biz>
2022-05-10 17:56:27 +0200

    Homepage view

---
+++ src/views/HomePageView.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container py-4">
+    <header class="pb-3 mb-4 border-bottom">
+
+        <span class="fs-4">Welcome</span>
+    </header>
+
+    <div class="p-5 mb-4 bg-light rounded-3">
+      <div class="container-fluid py-5">
+        <h1 class="display-5 fw-bold">Browse auctions</h1>
+        <p class="col-md-8 fs-4">See all the active auctions and make a bid on your desired items.
+            <br>Who dares wins.</p>
+        <button class="btn btn-primary btn-lg" type="button">Browse Auctions</button>
+      </div>
+    </div>
+
+    <div class="row align-items-md-stretch">
+      <div class="col-md-6">
+        <div class="h-100 p-5 text-white bg-dark rounded-3">
+          <h2>Start an auction</h2>
+          <p>Looking to sell your items for the best price possible? Starting an auction will most likely help you achieve your goal. Start it now on lazyBids!</p>
+          <button class="btn btn-outline-light" type="button">New Auction</button>
+        </div>
+      </div>
+      <div class="col-md-6">
+        <div class="h-100 p-5 bg-light border rounded-3">
+          <h2>Dashboard</h2>
+          <p>See all your ongoing auctions and bids in this beautiful dashboard</p>
+          <button class="btn btn-outline-secondary" type="button">Go to the dashboard</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
